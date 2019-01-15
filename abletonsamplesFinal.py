ac3236366ee655b1d98d5ad8119009607c7c23ba
StanislavAlexandrov <stanislavalexandrov@gmail.com>
2019-01-15 07:23:28 +0500

    Update abletonsamplesFinal.py
    Updated comments.

--- abletonsamplesFinal.py
+++ abletonsamplesFinal.py
@@ -8,17 +8,22 @@ from shutil import copyfile
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+#Ask user to select a project file. 
 Tk().withdraw()
 filename2 = askopenfilename()
+
+#Replace the .als extension with .zip and make a copy of the file. 
 base = os.path.splitext(filename2)[0]
 filename3 = copyfile(filename2, base + '.zip')
 
-
+#Unzip the resulting file. Only gzip worked. 
 with gzip.open(filename3, 'rb') as f:
 	read_data = f.read().decode('utf-8')
 
+	#Search for lines with the appropriate extension, turn the result into a set to exclude duplicates. 
 	searchResultWAV = re.findall("/\w+.*wav", read_data)
 	SetAndStringWAV = str((set(searchResultWAV)))
+	#Do a few replacements for readability.
 	Replace20WAV = SetAndStringWAV.replace("%20", "/")
 	FinalListWAV = Replace20WAV.replace(",", "\n")
 
@@ -34,7 +39,7 @@ with gzip.open(filename3, 'rb') as f:
 	Replace20AIFF = SetAndStringAIFF.replace("%20", "/")
 	FinalListAIFF = Replace20AIFF.replace(",", "\n")
 
-
+	#Print the results if they exist. 
 	if len(searchResultWAV) > 0:
 		print(FinalListWAV)
 	else:
@@ -51,7 +56,7 @@ with gzip.open(filename3, 'rb') as f:
 		print("No AIFF files found.")
 
 
-
+#Remove the copied file if it exists. 
 if os.path.exists(filename3):
   os.remove(filename3)
 else:
