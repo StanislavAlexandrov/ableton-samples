09192255d65b9e30269668154725dafdd55621b3
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2023-08-27 13:06:12 +0300

    Refactor and improve code readability and robustness
    - Encapsulated code into functions for modularity and ease of testing.
    - Added error handling for potential issues like file not found or file not being a gzip file.
    - Used f-strings for string formatting for better readability and efficiency.
    - Reduced code duplication by creating a function `search_and_print` to search for specified extensions in the read data and print the results.
    - Used context managers for file operations.
    - Moved file deletion logic to a `finally` block to ensure file gets deleted even if an error occurs.
    - Kept the original comments for clarity and context.

--- abletonsamplesFinal.py
+++ abletonsamplesFinal.py
@@ -1,78 +1,66 @@
 import os
 import re
-import sys
 import gzip
-
 from shutil import copyfile
-
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 
-#Ask user to select a project file. 
-Tk().withdraw()
-filename2 = askopenfilenames()
-print(filename2)
+def search_and_print(extension, read_data):
+    # Search for lines with the appropriate extension, turn the result into a set to exclude duplicates.
+    search_result = re.findall(f"/\w+.*{extension}", read_data)
+    set_and_string = str(set(search_result))
+    # Do a few replacements for readability.
+    replace_20 = set_and_string.replace("%20", "/")
+    final_list = replace_20.replace(",", "\n")
+    
+    # Print the results if they exist.
+    if search_result:
+        print(final_list)
+    else:
+        print(f"No {extension.upper()} files found.")
+    
 
-for file in filename2:
+def process_files(filename2):
+    for file in filename2:
+        print(str(file))
 
-	print(str(file))
+        # Replace the .als extension with .zip and make a copy of the file.
+        base = os.path.splitext(file)[0]
+        filename3 = copyfile(file, base + '.zip')
 
-	#Replace the .als extension with .zip and make a copy of the file. 
-	base = os.path.splitext(file)[0]
-	filename3 = copyfile(file, base + '.zip')
+        try:
+            # Unzip the resulting file. Only gzip worked.
+            with gzip.open(filename3, 'rb') as f:
+                read_data = f.read().decode('utf-8')
 
-	#Unzip the resulting file. Only gzip worked. 
-	with gzip.open(filename3, 'rb') as f:
-		read_data = f.read().decode('utf-8')
-
-		#Search for lines with the appropriate extension, turn the result into a set to exclude duplicates. 
-		searchResultWAV = re.findall("/\w+.*wav", read_data)
-		SetAndStringWAV = str((set(searchResultWAV)))
-		#Do a few replacements for readability.
-		Replace20WAV = SetAndStringWAV.replace("%20", "/")
-		FinalListWAV = Replace20WAV.replace(",", "\n")
-
-
-		searchResultMP3 = re.findall("/\w+.*mp3", read_data)
-		SetAndStringMP3 = str((set(searchResultMP3)))
-		Replace20MP3 = SetAndStringMP3.replace("%20", "/")
-		FinalListMP3 = Replace20MP3.replace(",", "\n")
-
-
-		searchResultAIFF = re.findall("/\w+.*aiff", read_data)
-		SetAndStringAIFF = str((set(searchResultAIFF)))
-		Replace20AIFF = SetAndStringAIFF.replace("%20", "/")
-		FinalListAIFF = Replace20AIFF.replace(",", "\n")
-
-		searchResultVST = re.findall("\w.*vst*", read_data)
-		#SetAndStringVST = str((set(searchResultAIFF)))
-		#Replace20VST = SetAndStringAIFF.replace("%20", "/")
-		#FinalListVST = Replace20AIFF.replace(",", "\n")
-
-		#Print the results if they exist. 
-		if len(searchResultWAV) > 0:
-			print(FinalListWAV)
-		else:
-			print("No WAV files found.")
-
-		if len(searchResultMP3) > 0:
-			print(FinalListMP3)
-		else:
-			print("No MP3 files found.")
-
-		if len(searchResultAIFF) > 0:
-			print(FinalListAIFF)
-		else:
-			print("No AIFF files found.")
-
-		if len(searchResultVST) > 0:
-			print(searchResultVST)
-		else:
-			print("No VST plugins found.")
+                search_and_print("wav", read_data)
+                search_and_print("mp3", read_data)
+                search_and_print("aiff", read_data)
+                
+                search_result_vst = re.findall("\w.*vst*", read_data)
+                if search_result_vst:
+                    print(search_result_vst)
+                else:
+                    print("No VST plugins found.")
+        except gzip.BadGzipFile:
+            print("The file is not a gzip file.")
+        except FileNotFoundError:
+            print("The file does not exist.")
+        finally:
+            # Remove the copied file if it exists.
+            if os.path.exists(filename3):
+                os.remove(filename3)
+            else:
+                print("The file does not exist and cannot be deleted.")
 
 
-#Remove the copied file if it exists. 
-		if os.path.exists(filename3):
-		  os.remove(filename3)
-		else:
-		  print("The file does not exist and cannot be deleted.")
+def main():
+    Tk().withdraw()
+    filename2 = askopenfilenames()
+    print(filename2)
+    
+    process_files(filename2)
+
+
+if __name__ == "__main__":
+    main()
